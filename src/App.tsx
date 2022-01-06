fecc3eabeef353746a0548944c614039f7065104
codegino <carloginocatapang@gmail.com>
2022-01-06 20:03:46 +0800

    fix(deps): fix dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import styled from '@emotion/styled';
-
-const Button = styled.button<{color123: string}>`
-  padding: 32px;
-  background-color: hotpink;
-  font-size: 24px;
-  border-radius: 4px;
-  color: ${prop => prop.color123};
-  font-weight: bold;
-  &:hover {
-    color: white;
-  }
-`;
+import logo from './logo.svg';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button color123="blue">ASD</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
